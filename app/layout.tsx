8b6ed0ffd9b900809ed65c1b5de3408af79be08a
aashish <quakie.exe@gmail.com>
2026-02-02 01:29:03 +0530

    addedd some minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "FURNIVO - Premium Furniture Store",
+  title: "Furnz.in - Premium Furniture Store",
   description: "Discover beautiful and affordable furniture for your home",
   icons: {
     icon: '/Furnz-logo.jpg'
--- app/page.tsx
+++ app/page.tsx
@@ -159,7 +159,7 @@ export default async function Home() {
               Discover What You Love
             </h2>
             <p className="text-gray-600 text-lg max-w-2xl mx-auto">
-              Handpicked furnitue pieces that combine style, comfort, and quality
+              Handpicked furniture pieces that combine style, comfort, and quality
             </p>
           </div>
 
